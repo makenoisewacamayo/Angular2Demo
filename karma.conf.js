3e57259b983acea3f3407a3775791d07b208422b
Rafael Avaria G <rafael.avaria@gmail.com>
2018-01-23 12:23:08 -0300

    Add pact test

--- karma.conf.js
+++ karma.conf.js
@@ -4,14 +4,23 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular/cli'],
+    frameworks: ['jasmine', '@angular/cli', 'pact'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular/cli/plugins/karma'),
+      require('@pact-foundation/karma-pact')
     ],
+    pact: [{
+      cors: true,
+      spec: 2,
+      port: 1234,
+      dir: 'pacts/'}],
+    proxies: {
+      '/api/product/all' : 'http://localhost:1234//api/product/all'
+    },
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
